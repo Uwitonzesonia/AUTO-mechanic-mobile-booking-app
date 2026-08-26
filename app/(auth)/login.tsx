c9fe9477f1dc9d52f063e87b405928d83ea811aa
Ishimwe William <ishimwe.william2000@gmail.com>
2026-08-26 19:08:51 +0200

    enhanced login and register screens with navigation links, added email/password login functionality, and updated header visibility in auth layout

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,16 +1,54 @@
-import {Button, Text, View} from "react-native";
+import {Button, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {useAuth} from "@/hooks/useAuth";
+import {useRouter} from "expo-router";
+import {AuthProps} from "@/types/auth";
+import {useState} from "react";
 
 const Login = () => {
-    const {loginWithGoogle} = useAuth()
+    const {loginWithGoogle, loginWithEmail, error} = useAuth()
+    const router = useRouter();
+    const [userData, setUserData] = useState<AuthProps>({email: '', password: ''});
 
     return (
         <View style={{flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center"}}>
             <Text>Login</Text>
+            {error && (
+                <Text style={{color: "red"}}>{error}</Text>
+            )}
+            <View style={{width: "80%"}}>
+                <Text>Email</Text>
+                <TextInput
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    textContentType="emailAddress"
+                    keyboardType="email-address"
+                    value={userData?.email}
+                    onChangeText={(email) => setUserData(prev => ({...prev, email}))}
+                    style={{marginBottom: 10, borderBottomWidth: 1, color: "black"}}
+                />
+                <Text>Password</Text>
+                <TextInput
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    value={userData?.password}
+                    onChangeText={(password) => setUserData(prev => ({...prev, password}))}
+                    autoCapitalize="none"
+                    style={{marginBottom: 10, borderBottomWidth: 1, color: "black"}}
+                />
+            </View>
+            <Button
+                title={"Login"}
+                onPress={() => loginWithEmail(userData)}
+            />
             <Button
                 title="Login with Google"
                 onPress={loginWithGoogle}
             />
+            <TouchableOpacity
+                onPress={() => router.replace("/(auth)/register")}
+            >
+                <Text>Register</Text>
+            </TouchableOpacity>
         </View>
     )
 }
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,16 +1,23 @@
-import {Button, Text, View} from "react-native";
+import {Button, Text, TouchableOpacity, View} from "react-native";
 import {useAuth} from "@/hooks/useAuth";
+import {useRouter} from "expo-router";
 
 const Register = () => {
     const {loginWithGoogle} = useAuth()
+    const router = useRouter();
 
     return (
         <View style={{flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center"}}>
             <Text>Register</Text>
             <Button
-                title="Register"
+                title="Login with Google"
                 onPress={loginWithGoogle}
             />
+            <TouchableOpacity
+                onPress={() => router.replace("/(auth)/login")}
+            >
+                <Text>Login</Text>
+            </TouchableOpacity>
         </View>
     )
 }
